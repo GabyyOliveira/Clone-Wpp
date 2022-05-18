f59be46d65391ff40c6e996b31bfb0e3108e2c9a
Gabs <gabyy.oliveira33@gmail.com>
2022-05-18 19:05:18 -0300

    construindo o layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React ,{useState, useEffect} from 'react';
 import './App.css';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchIcon from '@material-ui/icons/Search'
+import ChatListItem from './components/ChatListItem';
+import ErrorIcon from '@material-ui/icons/Error';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const App = () =>{
+
+  const [chatList, setChatList] = useState([{}, {}, {}, {}])
+  return(
+    <div className='app-window'>
+      <div className='sidebar'>
+        <header>
+          <img className="header-avatar" src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
+          <div className='header-buttons'>
+            <div className='header-btn'>
+              <DonutLargeIcon style={{color: '#919191'}} />
+            </div>
+            <div className='header-btn'>
+              <ChatIcon style={{color: '#919191'}} />
+            </div>
+            <div className='header-btn'>
+              <MoreVertIcon style={{color: '#919191'}} />
+            </div>
+          </div>
+        </header>
+        <div className='barra'>
+            <ErrorIcon fontSize='large' style={{color: '#53BDEB'}} />
+          <div className='barra-text'>
+            <p>Projeto Clone App Whatsapp<br /> <a href="">Ativar notificações na átrea de trabalho</a></p>
+            
+          </div>
+        </div>
+        <div className='search'>
+          <div className='search-input'>
+            <SearchIcon fontSize='small' style={{color: '#919191'}}/>
+            <input type="search" placeholder="Procurar ou começar uma nova conversa" />
+          </div>
+        </div>
+        <div className='chatList'>
+          {chatList.map((item, key)=>(
+            <ChatListItem key={key} />
+          ))}
+        </div>
+      </div>
+      <div className='contentarea'>
+        ...
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
